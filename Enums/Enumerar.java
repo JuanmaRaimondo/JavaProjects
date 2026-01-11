ae6fdf70272bd750d6f52e0cd8b5fda7f4fe0b87
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-01-11 20:38:36 -0300

    Se agrega practica de enum

---
+++ Enums/Enumerar.java
@@ -0,0 +1,10 @@
+package JavaProjects.Enums;
+
+public class Enumerar {
+
+    enum Color{ ROJO, AZUL, VERDE}
+    public static void main(String[] args) {
+        Color color = Color.ROJO;
+        System.out.println(color);
+    }
+}
